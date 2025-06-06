a2c3093d78348c847fab2843dc3855fda1c5a0ef
Meniou56 <manu270687@gmail.com>
2025-06-06 10:44:12 +0200

    activation tooltips

--- script.js
+++ script.js
@@ -1,92 +1,108 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Observer les changements de visibilité pour différents éléments
-    let observer = new IntersectionObserver((entries, observer) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                // Si l'élément est la machine à écrire
-                if (entry.target.id === "machineAecrire") {
-                    machineAEcrire(entry.target)
-                }
-                // Si l'élément est une barre de progression
-                else if (entry.target.classList.contains("progress-bar")) {
-                    animateBarresProgress(entry.target)
-                }
-                // Désinscrit l'élément après l'animation pour ne pas répéter
-                observer.unobserve(entry.target)
-            }
-        })
-    }, {
-        root: null,
-        rootMargin: '0px',
-        threshold: 1
-    })
-
-    // Machine à écrire
-    const texte = "Hello World!"
-    let i = 0
-
-    function machineAEcrire(elem) {
-        function typeWriter() {
-            if (i < texte.length) {
-                elem.innerHTML += texte.charAt(i)
-                i++
-                setTimeout(typeWriter, getRandomInt(50, 300))
-            }
+document.addEventListener("DOMContentLoaded", function () {
+  // Observer les changements de visibilité pour différents éléments
+  let observer = new IntersectionObserver(
+    (entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          // Si l'élément est la machine à écrire
+          if (entry.target.id === "machineAecrire") {
+            machineAEcrire(entry.target);
+          }
+          // Si l'élément est une barre de progression
+          else if (entry.target.classList.contains("progress-bar")) {
+            animateBarresProgress(entry.target);
+          }
+          // Désinscrit l'élément après l'animation pour ne pas répéter
+          observer.unobserve(entry.target);
         }
-        typeWriter()
+      });
+    },
+    {
+      root: null,
+      rootMargin: "0px",
+      threshold: 1,
     }
+  );
 
-    // Animer les barres de progression
-    function animateBarresProgress(barre) {
-        barre.style.width = "0"; 
+  // Machine à écrire
+  const texte = "Hello World!";
+  let i = 0;
+
+  function machineAEcrire(elem) {
+    function typeWriter() {
+      if (i < texte.length) {
+        elem.innerHTML += texte.charAt(i);
+        i++;
+        setTimeout(typeWriter, getRandomInt(50, 300));
+      }
+    }
+    typeWriter();
+  }
+
+  // Animer les barres de progression
+  function animateBarresProgress(barre) {
+    barre.style.width = "0";
+
+    setTimeout(() => {
+      const widthPercent = barre.dataset.target + "%";
+      barre.style.width = widthPercent;
+    }, 100);
+  }
+
+  // Générateur aléatoire pour l'effet machine à écrire
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  // Observer l'élément machine à écrire
+  const elemMachineAecrire = document.getElementById("machineAecrire");
+  if (elemMachineAecrire) {
+    observer.observe(elemMachineAecrire);
+  }
+
+  // Observer chaque barre de progression
+  document.querySelectorAll(".progress-bar").forEach((bar) => {
+    observer.observe(bar);
+  });
+
+  // Bouton de scroll UP
+  const btnUp = document.getElementById("btnUP");
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 300) {
+      btnUp.style.display = "block";
+    } else {
+      btnUp.style.display = "none";
+    }
+  });
+  
+  /* Formulaire de contact */
+  (function () {
+      "use strict";
+      document.addEventListener("DOMContentLoaded", function () {
+          const form = document.getElementById("contactForm");
+          
+          form.addEventListener(
+              "submit",
+              function (event) {
+                  if (!form.checkValidity()) {
+                      event.preventDefault(); // Empêche l'envoi si invalide
+                      event.stopPropagation();
+                    }
+                    form.classList.add("was-validated"); // Active les styles Bootstrap
+                },
+                false
+            );
+        });
+    })();
     
-        setTimeout(() => {
-            const widthPercent = barre.dataset.target + "%";
-            barre.style.width = widthPercent;
-        }, 100);
-    }
+    /* Activation tooltips */
+    const tooltipTriggerList = document.querySelectorAll(
+        '[data-bs-toggle="tooltip"]'
+    );
+    const tooltipList = [...tooltipTriggerList].map(
+        (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+    );
     
-    // Générateur aléatoire pour l'effet machine à écrire
-    function getRandomInt(min, max) {
-        min = Math.ceil(min)
-        max = Math.floor(max)
-        return Math.floor(Math.random() * (max - min + 1)) + min
-    }
-
-    // Observer l'élément machine à écrire
-    const elemMachineAecrire = document.getElementById("machineAecrire")
-    if (elemMachineAecrire) {
-        observer.observe(elemMachineAecrire)
-    }
-
-    // Observer chaque barre de progression
-    document.querySelectorAll('.progress-bar').forEach(bar => {
-        observer.observe(bar)
-    })
-
-    // Bouton de scroll UP
-    const btnUp = document.getElementById("btnUP");
-    window.addEventListener("scroll", function () {
-        if (window.scrollY > 300) {
-            btnUp.style.display = "block";
-        } else {
-            btnUp.style.display = "none";
-        }
-    });
 });
-
-/* Formulaire de contact */
-(function () {
-    'use strict';
-    document.addEventListener('DOMContentLoaded', function () {
-      const form = document.getElementById('contactForm');
-
-      form.addEventListener('submit', function (event) {
-        if (!form.checkValidity()) {
-          event.preventDefault();  // Empêche l'envoi si invalide
-          event.stopPropagation();
-        }
-        form.classList.add('was-validated'); // Active les styles Bootstrap
-      }, false);
-    });
-  })();
